9bebd5a61b54270064c842d47ec49ed80395ab5e
pawan-live <pawan.senpura@gmail.com>
2022-02-15 00:07:27 +0530

    tute 3

--- Tute03.c
+++ Tute03.c
@@ -8,12 +8,18 @@ e.g.
 n -> 100
 sum = 1+2+3+....+ 99+100 = 5050
 
-n -> 1-
+n -> 10
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
+  int num, total = 0;
+  printf("Enter number: ");
+  scanf("%d", &num);
+  for (int i = 0; i <= num; i++) {
+    total = total + i;
+  }
+  printf("%d", total);
   return 0;
 }
 
