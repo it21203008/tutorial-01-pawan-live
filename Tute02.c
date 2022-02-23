7b097dd5224f360231c7941fc3fe7fcd51ec1eca
it21203008 <99604582+it21203008@users.noreply.github.com>
2022-02-23 11:40:08 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -1,36 +1,21 @@
-/*Exercise 2 - Selection
-
-Write a program to calculate the amount to be paid for a rented vehicle.
-
-•	Input the distance the van has travelled
-•	The first 30 km is at a rate of 50/= per km.
-•	The remaining distance is calculated at the rate of 40/= per km.
-
-
-e.g.
-
-Distance -> 20
-Amount = 20 x 50 = 1000
-
-
-Distance -> 50
-Amount = 30 x 50 + (50-30) x 40 = 2300*/
-
-#include <stdio.h>
-#define normalRate 50
-#define extraRate 40
-
-int main() {
-  float distance;
-  float finalFare = 0;
-  printf("Enter distance: ");
-  scanf("%f", &distance);
-  if (distance <= 30 && distance > 0) {
-    finalFare = distance * normalRate;
-  } else if (distance > 30) {
-    finalFare = (normalRate * 30) + ((distance - 30) * extraRate); 
-  }
-
-  printf("Your fare is: %.2f", finalFare);
-  return 0;
+#include<stdio.h>
+int main()
+{
+	int x;//distance
+	float amount;
+	printf("input the distance the vehicle has travelled :");
+	scanf("%d",&x);
+	
+	if(x>=30)
+	{
+		amount=(30*50)+(x-30)*40;
+	}
+	else
+	{
+		amount=x*50;
+	}
+	
+	printf("paid for a rented vehicle :%.2f",amount);
+	
+	return 0;
 }
